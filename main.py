be57cfe75def21dceb7d542537084645d516d4a4
Vasandre <125184233+Vasandre@users.noreply.github.com>
2023-02-12 14:42:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def addition(first_num, second_num):
+    return first_num + second_num
+
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+print(addition(a, b))
