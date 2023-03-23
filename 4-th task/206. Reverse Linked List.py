464ff9539af40ca516f5b9b3b70ede997e70b430
Vasandre <125184233+Vasandre@users.noreply.github.com>
2023-03-23 21:36:17 +0300

    Update 206. Reverse Linked List.py

--- 4-th task/206. Reverse Linked List.py
+++ 4-th task/206. Reverse Linked List.py
@@ -3,6 +3,9 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# https://leetcode.com/problems/reverse-linked-list/
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
