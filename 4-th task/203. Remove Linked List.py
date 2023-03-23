c79caa49c7dce070f445430ad303f3986b72c234
Vasandre <125184233+Vasandre@users.noreply.github.com>
2023-03-23 21:35:47 +0300

    Update 203. Remove Linked List.py

--- 4-th task/203. Remove Linked List.py
+++ 4-th task/203. Remove Linked List.py
@@ -3,6 +3,9 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# https://leetcode.com/problems/remove-linked-list-elements/
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
 
